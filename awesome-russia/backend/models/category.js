1c8db3d779902b1bfee151df1759caa415c91a24
lowwin-dev <low111win@gmail.com>
2026-04-20 01:19:49 +0600

    Add getAllCategories function to retrieve categories

--- awesome-russia/backend/models/category.js
+++ awesome-russia/backend/models/category.js
@@ -1,1 +1,8 @@
+const pool = require('../db');
 
+async function getAllCategories() {
+  const res = await pool.query('SELECT * FROM categories ORDER BY sort_order');
+  return res.rows;
+}
+
+module.exports = { getAllCategories };
